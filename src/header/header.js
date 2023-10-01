350aea08d06d2ae2f18cd40c57b3294738a80b70
RaviHarish <raviharish108@gmail.com>
2023-10-01 12:57:52 +0530

    logout button

--- src/header/header.js
+++ src/header/header.js
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 export function Header() {
 const navigate=useNavigate();
+const token = localStorage.getItem('user-token');
+
+ navigate("/home");
 const logout = () => {
   localStorage.clear();
   navigate('/');
@@ -11,8 +14,8 @@ const logout = () => {
          <ul>
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/">Login</Link></li>
-          <li ><button onClick={()=>{
-                                 logout() }}>Logout</button></li>
+          {token?<li ><button onClick={()=>{logout() }}>Logout</button></li>:null}
+          
          </ul>
     </div>
     )
